720f982929ce5a71ecd073dc6857cbd87bd58fed
moulali39 <107177595+moulali39@users.noreply.github.com>
2022-07-27 22:53:19 +0530

    Add files via upload

---
+++ conditional_statement.py
@@ -0,0 +1,72 @@
+# a=1,2,3,4
+# for mouli in a:
+#     print(mouli,end=" ")
+
+
+# for i in range (5):
+#     print(i,end=" ")
+
+# n=int(input("enter the value of n:"))
+# for i in range (1,1+n):
+#     print(i)
+
+# a=int(input("enter the value of a:"))
+# for i in range (1,1+a):
+#     print(i)
+
+# n=int(input("enter the value n:"))
+# for i in range (1,n,2):
+#     print(i)
+    
+    
+# n=int(input("enter the value n:"))
+# for i in range (2,n,2):
+#     print(i,end=" ")
+
+
+# n=int(input("enter the value n:"))
+# for i in range (25,n,2):
+#     print(i,end=" ")
+
+# """""""" 1. even or odd number values  """"""
+
+
+# n=int(input("enter the value of n:"))
+# if (n%2==0):
+#     print(f"value is even number")
+# else:
+#     print(f"value is odd number")    
+
+# for i in range (5):
+#     print(i*i)
+   
+# n=int(input("enter the value of n:"))    
+# for i in range (n):
+#     print(i*i*i)
+
+# """"""" 2.while loop     """"""""
+
+# a=int(input("enter the value of a:"))
+# while(a<=12):
+#     print("hello",str(a))
+    # a+=1
+
+# while (1<=10):
+#  print("hello")
+
+# n=int(input("enter the value of n:"))
+# while(n<=10):
+#     print("hello",str(n))
+#     n+=1
+
+# a=int(input("enter the value of a:"))
+# if (a==5):
+#     print("value is equal")
+
+# a,b=[int (x) for x in input().split()]
+# if(a>b):print("a is greter then b")
+# if(a<b):print("a is less then b")
+# if(a<=b):print("a is less then or equal to b")
+# if(a>=b):print("a is greater then or equal to b")
+# if(a!=b):print("a is not equal to b")
+# if(a==b):print("a is not equal to b")
